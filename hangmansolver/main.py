cff6468c0c7a08061c18c852dd344f32fd7cd185
Yusuf Ahmed <Yusuf.Ahmed.6@city.ac.uk>
2025-05-17 17:25:55 +0100

    rewrote in subroutine form

--- hangmansolver/main.py
+++ hangmansolver/main.py
@@ -1,40 +1,42 @@
 import string, time
 
-# Define all characters to use in the password
-chars = string.ascii_lowercase
-print(chars)
+def run():
+    # Define all characters to use in the password
+    chars = string.ascii_lowercase
+    print(chars)
 
-# Define the password to be cracked
-word = input("Input partial solution:")
+    # Define the password to be cracked
+    word = input("Input partial solution:")
 
-# Define max_length, min_length length and passwordfound
-max_length = len(word)
-min_length = len(word)
-passwordfound = False
+    # Define max_length, min_length length and passwordfound
+    max_length = len(word)
+    min_length = len(word)
+    passwordfound = False
 
-# Track the start time of the password cracking process
-start_time = time.perf_counter()
+    # Track the start time of the password cracking process
+    start_time = time.perf_counter()
 
-loops = 0
+    loops = 0
 
-for char in word:
-    if char == "_":
-        loops += 1
+    for char in word:
+        if char == "_":
+            loops += 1
 
-tempLOW = []
-ListOfWords = []
-
-for char in chars:
-    tempword = word.replace("_", char, 1)
-    tempLOW.append(tempword)
-
-for c in range(loops-1):
-    ListOfWords = tempLOW
     tempLOW = []
+    ListOfWords = []
 
-    for word in ListOfWords:
-        for char in chars:
-            tempword = word.replace("_", char, 1)
-            tempLOW.append(tempword)
+    for char in chars:
+        tempword = word.replace("_", char, 1)
+        tempLOW.append(tempword)
 
-print(tempLOW)
+    for c in range(loops-1):
+        ListOfWords = tempLOW
+        tempLOW = []
+
+        for word in ListOfWords:
+            for char in chars:
+                tempword = word.replace("_", char, 1)
+                tempLOW.append(tempword)
+
+    print(tempLOW)
+    return(tempLOW)
